27e153bfbb3938e246678b407e6c6c04d86ce2c9
Sibin Xavier <sibinx7@gmail.com>
2016-01-25 09:37:20 +0530

    Support for Fat functions
    stage-0 preset added to get support for fat arrows ( ES 2015 feature )

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,21 @@ var browserify = require("browserify")
 var babelify = require("babelify")
 var gutil = require('gulp-util');
 var source = require('vinyl-source-stream');
+var livereload = require('gulp-livereload');
 gulp.task("react",function(){
   return browserify({
     entries:'./app/assets/javascripts/react/main.js',
     debug: true 
   })
-  .transform("babelify",{presets:["react","es2015"]})
+  .transform("babelify",{presets:["react","es2015","stage-0"],plugins:["transform-es2015-arrow-functions"]})
   .bundle()
   .pipe(source("bundle.js"))
   .pipe(gulp.dest("./app/assets/javascripts"))
+  .pipe(livereload())
 })
 
 gulp.task("watch",["react"],function(){
-  gulp.watch("./app/assets/javascripts/react/*.js",["react"])
+  livereload.listen({basePath:'public'})
+  gulp.watch("./app/assets/javascripts/react/*/*.js",["react"])
 })
-gulp.task("default",["react","watch"])
+gulp.task("default",["watch"])
